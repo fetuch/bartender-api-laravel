7a9cca68fdd9dca3aba021738e85a0d7a5587aca
Fetuch <rafal.nowak@punkgate.pl>
2023-02-28 20:21:42 +0100

    Test invalid category creation

--- src/bartender/tests/Feature/Drink/CreateDrinkTest.php
+++ src/bartender/tests/Feature/Drink/CreateDrinkTest.php
@@ -16,3 +16,37 @@ it('should create a drink', function () {
         ->attributes->instructions->toBe('Drink instructions');
 })->group('drink', 'create-drink');
 
+it('should return 422 if name is invalid', function (?string $name) {
+    postJson(route('drinks.store'), [
+        'name' => $name,
+        'instructions' => 'instructions',
+        'categoryId' => Category::factory()->create()->uuid,
+    ])->assertInvalid(['name']);
+})->with([
+    null,
+    '',
+])->group('drink', 'create-drink');
+
+it('should return 422 if instructions are invalid', function (?string $instructions) {
+    postJson(route('drinks.store'), [
+        'instructions' => $instructions,
+        'name' => 'Drink name',
+        'categoryId' => Category::factory()->create()->uuid,
+    ])->assertInvalid(['instructions']);
+})->with([
+    null,
+    '',
+])->group('drink', 'create-drink');
+
+it('should return 422 if category is invalid', function (?string $categoryID) {
+    postJson(route('drinks.store'), [
+        'categoryID' => $categoryID,
+        'instructions' => 'instructions',
+        'name' => 'drink name',
+    ])->assertInvalid(['categoryId']);
+})->with([
+    'invalid-category-id',
+    null,
+    '',
+])->group('drink', 'create-drink');
+
